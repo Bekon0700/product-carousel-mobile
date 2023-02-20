cad0f323b3ad01756e01a76b531c5b73fe713419
Emdadul <emdadul5023@onnorokom.com>
2023-02-20 17:31:53 +0600

    done

--- js/videoViewer.js
+++ js/videoViewer.js
@@ -5,6 +5,8 @@ const video = document.getElementById('video')
 img.addEventListener('click', () => {
     video.classList.add('active')
     $('.carousel_container').addClass('d-none')
+    const p = $('iframe').attr('src')
+    console.log(p);
 })
 
 crossBtn.addEventListener('click', () => {
